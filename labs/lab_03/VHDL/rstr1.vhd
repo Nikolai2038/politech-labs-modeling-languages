5edad8346f25d9125a92f9c5989dfbf8f3744325
Nikolai2038 <nikolai2038@yandex.ru>
2023-09-26 20:54:57 +0300

    Fix RS trigger VHDL

--- labs/lab_03/VHDL/rstr1.vhd
+++ labs/lab_03/VHDL/rstr1.vhd
@@ -14,14 +14,14 @@ ARCHITECTURE behav OF rstr1 IS
 BEGIN
     PROCESS (s,r)
     BEGIN
-        IF s=’1’ THEN
-            IF r=’1’ THEN
+        IF s='1' THEN
+            IF r='1' THEN
                 qs<=qs;
             ELSE
-                qs<=’0’;
+                qs<='0';
             END IF;
         ELSE
-            qs<=’1’;
+            qs<='1';
         END IF;
     END PROCESS;
     q<=qs;
