7f2166046c010191e145245661a2f6fb34f025a8
Nikolai2038 <nikolai2038@yandex.ru>
2023-10-02 00:03:47 +0300

    Lab 4 point 8 done

--- labs/lab_04/p2_vhdl/p3_reg_shift_2stroke_b.vhd
+++ labs/lab_04/p2_vhdl/p3_reg_shift_2stroke_b.vhd
@@ -1,1 +1,49 @@
+-- Подключение библиотеки ieee
+LIBRARY ieee;
 
+-- Использование библиотечного модуля, содержащего дополнительные типы переменных
+USE ieee.std_logic_1164.ALL;
+
+ENTITY p3_reg_shift_2stroke_b IS
+    -- Описание входов и выходов устройства
+    PORT (
+        d : IN std_logic;
+        c1 : IN std_logic;
+        c2 : IN std_logic;
+        r : IN std_logic;
+        q1 : OUT std_logic;
+        q2 : OUT std_logic;
+        q3 : OUT std_logic;
+        q4 : OUT std_logic
+    );
+END p3_reg_shift_2stroke_b;
+
+ARCHITECTURE p3_reg_shift_2stroke_b_behaviour OF p3_reg_shift_2stroke_b IS
+    SIGNAL qs1:std_logic;
+    SIGNAL qs2:std_logic;
+    SIGNAL qs3:std_logic;
+    SIGNAL qs4:std_logic;
+BEGIN
+    PROCESS (d) BEGIN
+        IF r = '0' THEN
+            qs1 <= '0';
+            qs2 <= '0';
+            qs3 <= '0';
+            qs4 <= '0';
+        ELSE
+            IF c1 = '1' THEN
+                qs1 <= d;
+                qs3 <= qs2;
+            END IF;
+            IF c2 = '1' THEN
+                qs2 <= qs1;
+                qs4 <= qs3;
+            END IF;
+        END IF;
+    END PROCESS;
+
+    q1 <= qs1;
+    q2 <= qs2;
+    q3 <= qs3;
+    q4 <= qs4;
+END p3_reg_shift_2stroke_b_behaviour;
