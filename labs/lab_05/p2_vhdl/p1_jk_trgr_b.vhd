5116a71fbfadb2887e5c7e996939fe1dd3ea73ff
Nikolai2038 <nikolai2038@yandex.ru>
2023-10-17 18:57:16 +0300

    Fix lab 5 vhdl part 2, attempt on lab 5 part 1

--- labs/lab_05/p2_vhdl/p1_jk_trgr_b.vhd
+++ labs/lab_05/p2_vhdl/p1_jk_trgr_b.vhd
@@ -6,41 +6,29 @@ ENTITY p1_jk_trgr_b IS
     PORT (
         j : IN std_logic;
         k : IN std_logic;
-        q1 : OUT std_logic;
-        q2 : OUT std_logic
+        reset : IN std_logic;
+        q : OUT std_logic
     );
 END p1_jk_trgr_b;
 
 ARCHITECTURE p1_jk_trgr_b_behaviour OF p1_jk_trgr_b IS
-    SIGNAL qs1:std_logic;
-    SIGNAL qs2:std_logic;
+    SIGNAL qs:std_logic;
 BEGIN
-    PROCESS BEGIN
-        IF r = '0' THEN
-            qs1 <= '0';
-            qs3 <= '0';
+    PROCESS (reset, j, k) BEGIN
+        IF reset = '1' THEN
+            qs <= '0';
         ELSE
-            IF c1 = '1' THEN
-                qs1 <= d;
-                qs3 <= qs2;
+            IF j = '0' AND k = '0' THEN
+                qs <= qs;
+            ELSIF j = '0' AND k = '1' THEN
+                qs <= '0';
+            ELSIF j = '1' AND k = '0' THEN
+                qs <= '1';
+            ELSIF j = '1' AND k = '1' AND j'event THEN
+                qs <= NOT qs;
             END IF;
         END IF;
     END PROCESS;
 
-    PROCESS (c2, r) BEGIN
-        IF r = '0' THEN
-            qs2 <= '0';
-            qs4 <= '0';
-        ELSE
-            IF c2 = '1' THEN
-                qs2 <= qs1;
-                qs4 <= qs3;
-            END IF;
-        END IF;
-    END PROCESS;
-
-    q1 <= qs1;
-    q2 <= qs2;
-    q3 <= qs3;
-    q4 <= qs4;
+    q <= qs;
 END p1_jk_trgr_b_behaviour;
--- labs/lab_05/p2_vhdl/p2_t_trgr_b.vhd
+++ labs/lab_05/p2_vhdl/p2_t_trgr_b.vhd
@@ -11,21 +11,15 @@ ENTITY p2_t_trgr_b IS
 END p2_t_trgr_b;
 
 ARCHITECTURE p2_t_trgr_b_behaviour OF p2_t_trgr_b IS
-    SIGNAL was_front:std_logic;
     SIGNAL qs:std_logic;
 BEGIN
     PROCESS (reset, t) BEGIN
         IF reset = '1' THEN
-            was_front <= '0';
             qs <= '0';
         ELSE
-            IF t = '1' THEN
-                IF was_front = '0' THEN
-                    qs <= NOT qs;
-                END IF;
-                was_front <= '1';
+            IF t = '1' AND t'event THEN
+                qs <= NOT qs;
             ELSE
-                was_front <= '0';
                 qs <= qs;
             END IF;
         END IF;
