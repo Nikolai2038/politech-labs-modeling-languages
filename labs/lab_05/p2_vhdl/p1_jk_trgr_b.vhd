f9d5e06d2f0aeff1887d79dc0051415ab45d23f3
Nikolai2038 <nikolai2038@yandex.ru>
2023-10-17 19:01:02 +0300

    Fix lab 5 vhdl part 2

--- labs/lab_05/p2_vhdl/p1_jk_trgr_b.vhd
+++ labs/lab_05/p2_vhdl/p1_jk_trgr_b.vhd
@@ -7,6 +7,7 @@ ENTITY p1_jk_trgr_b IS
         j : IN std_logic;
         k : IN std_logic;
         reset : IN std_logic;
+        clock : IN std_logic;
         q : OUT std_logic
     );
 END p1_jk_trgr_b;
@@ -14,19 +15,21 @@ END p1_jk_trgr_b;
 ARCHITECTURE p1_jk_trgr_b_behaviour OF p1_jk_trgr_b IS
     SIGNAL qs:std_logic;
 BEGIN
-    PROCESS (reset, j, k) BEGIN
+    PROCESS (reset, j, k, clock) BEGIN
         IF reset = '1' THEN
             qs <= '0';
-        ELSE
+        ELSIF clock = '1' AND clock'event THEN
             IF j = '0' AND k = '0' THEN
                 qs <= qs;
             ELSIF j = '0' AND k = '1' THEN
                 qs <= '0';
             ELSIF j = '1' AND k = '0' THEN
                 qs <= '1';
-            ELSIF j = '1' AND k = '1' AND j'event THEN
+            ELSIF j = '1' AND k = '1' THEN
                 qs <= NOT qs;
             END IF;
+        ELSE
+            qs <= qs;
         END IF;
     END PROCESS;
 
