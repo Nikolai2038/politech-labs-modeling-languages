6db6553031ee9102ffface5d5d8e28410d8fb512
Nikolai2038 <nikolai2038@yandex.ru>
2023-10-24 19:24:31 +0300

    Development

--- labs/lab_06/p2_vhdl/p2_dc_6to64.vhd
+++ labs/lab_06/p2_vhdl/p2_dc_6to64.vhd
@@ -1,6 +1,7 @@
 LIBRARY IEEE;
 
 USE IEEE.std_logic_1164.ALL;
+use IEEE.std_logic_arith.ALL;
 
 ENTITY p2_dc_6to64 IS
     PORT (
@@ -19,8 +20,18 @@ BEGIN
         ELSE
             qs <= (others => '0');
             CASE a IS
-                WHEN "000001" => qs <= (others => '0');
-                WHEN "111110" => qs(63) <= '1';
+                WHEN "000001" => qs(0) <= '1';
+                WHEN "000010" => qs(1) <= '1';
+                WHEN "000011" => qs(2) <= '1';
+                WHEN "000100" => qs(3) <= '1';
+                WHEN "000101" => qs(4) <= '1';
+                WHEN "000110" => qs(5) <= '1';
+                WHEN "000111" => qs(6) <= '1';
+                -- ...
+                WHEN "111100" => qs(60) <= '1';
+                WHEN "111101" => qs(61) <= '1';
+                WHEN "111110" => qs(62) <= '1';
+                WHEN "111111" => qs(63) <= '1';
                 WHEN OTHERS   => qs <= (others => '1');
             END CASE;
         END IF;
